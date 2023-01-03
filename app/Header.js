c3cbfba089f318a6962fc0b113b9840b873d339e
James Donald <dev@chaoscantrip.com>
2023-01-03 22:09:17 +0000

    Added header CSS

--- app/Header.js
+++ app/Header.js
@@ -1,6 +1,8 @@
+import styles from "./Header.module.css";
+
 export default function Header() {
     return (
-        <header>
+        <header className={styles.header_class}>
             <h1>This is the header</h1>
         </header>
     )
