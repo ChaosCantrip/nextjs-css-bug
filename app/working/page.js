4a3fcde59ab6176bb173b9d5130a4f9ce5137274
James Donald <dev@chaoscantrip.com>
2023-01-03 23:00:57 +0000

    Added working page

---
+++ app/working/page.js
@@ -0,0 +1,9 @@
+import styles from "../Header.module.css";
+
+export default function WorkingPage() {
+    return (
+        <div>
+            <p>This is the Working Page</p>
+        </div>
+    )
+}
