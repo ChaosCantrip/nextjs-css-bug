f0f6e10c62e3cc2310ee25d46948a8128f3e02be
James Donald <dev@chaoscantrip.com>
2023-01-03 23:03:07 +0000

    Added links

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function HomePage() {
     return (
         <div>
             <p>This is the Home Page</p>
+            <Link href={"/working"}>Link to /working, where styles apply</Link>
+            <p>If the style applies when coming back from /working, refresh the page to reload styles</p>
         </div>
     )
 }
--- app/working/page.js
+++ app/working/page.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 import styles from "../Header.module.css";
 
 export default function WorkingPage() {
     return (
         <div>
             <p>This is the Working Page</p>
+            <Link href={"/"}>Link back to home (broken styles)</Link>
         </div>
     )
 }
